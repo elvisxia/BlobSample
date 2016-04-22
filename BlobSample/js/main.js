f654423a5168c9d03adba0c9c1e91b5890f0b802
Xuchen.Xia <xiaxucheng@126.com>
2016-04-22 09:23:58 +0800

    Add project files.

---
+++ BlobSample/js/main.js
@@ -0,0 +1,47 @@
+﻿// Your code here!
+(function () {
+    "use strict"
+
+    window.URL = window.URL || window.webkitURL;
+
+    var fileSelect = document.getElementById("fileSelect"),
+        fileElem = document.getElementById("fileElem"),
+        fileList = document.getElementById("fileList");
+
+    fileSelect.addEventListener("click", function (e) {
+        if (fileElem) {
+            fileElem.click();
+        }
+        e.preventDefault(); // prevent navigation to "#"
+    }, false);
+
+    fileElem.onchange=function(){
+        handleFiles(this.files);
+    }
+
+    function handleFiles(files) {
+        if (!files.length) {
+            fileList.innerHTML = "<p>No files selected!</p>";
+        } else {
+            fileList.innerHTML = "";
+            var list = document.createElement("ul");
+            fileList.appendChild(list);
+            for (var i = 0; i < files.length; i++) {
+                var li = document.createElement("li");
+                list.appendChild(li);
+
+                var audio = document.createElement("audio");
+                audio.src = window.URL.createObjectURL(files[i]);//here to create the object url. It is in format of blob:E2B122AA-5FD4-41F5-9C87-944A68AF7456
+                audio.height = 60;
+                audio.onload = function () {
+                    window.URL.revokeObjectURL(this.src);
+                }
+                audio.play();
+                li.appendChild(audio);
+                var info = document.createElement("span");
+                info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+                li.appendChild(info);
+            }
+        }
+    }
+})();
